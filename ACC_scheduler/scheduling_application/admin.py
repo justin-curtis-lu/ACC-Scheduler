4ee53e3905e594b72b383dbff5f55f6eb4be4277
timothy-tai <titai@ucsc.edu>
2021-07-29 11:04:18 -0700

    Added senior model

--- ACC_scheduler/scheduling_application/admin.py
+++ ACC_scheduler/scheduling_application/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from scheduling_application.models import Senior
 
 # Register your models here.
+@admin.register(Senior)
+class SeniorAdmin(admin.ModelAdmin):
+    pass
--- ACC_scheduler/scheduling_application/models.py
+++ ACC_scheduler/scheduling_application/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Senior(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
