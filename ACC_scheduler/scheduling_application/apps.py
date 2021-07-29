84857e0eb16bd481fe96a8c954644d919c2d73cd
Nate Yeo <nathan@theyeos.com>
2021-07-29 13:23:48 -0700

    added verbose name

--- ACC_scheduler/scheduling_application/apps.py
+++ ACC_scheduler/scheduling_application/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SchedulingApplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scheduling_application'
+
+    # changes the header name on the admin site
+    verbose_name = 'Users Database'         # MAY NEED TO CHANGE IF USED ELSEWHERE
