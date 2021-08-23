43f6d54e98775f2be8e8d82165cdbe0fe2a73752
timothy-tai <titai@ucsc.edu>
2021-08-23 12:06:56 -0700

    adjusted availability model

--- ACC_scheduler/scheduling_application/models.py
+++ ACC_scheduler/scheduling_application/models.py
@@ -25,6 +25,46 @@ class Senior(models.Model):
     def __str__(self):
         return self.full_name
 
+class Day(models.Model):
+    _9_10 = models.BooleanField(default=False)
+    _10_11 = models.BooleanField(default=False)
+    _11_12 = models.BooleanField(default=False)
+    _12_1 = models.BooleanField(default=False)
+    _1_2 = models.BooleanField(default=False)
+    all = models.BooleanField(default=False)
+
+class Availability(models.Model):
+    _1 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _2 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _3 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _4 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _5 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _6 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _7 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _8 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _9 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _10 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _11 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _12 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _13 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _14 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _15 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _16 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _17 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _18 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _19 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _20 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _21 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _22 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _23 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _24 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _25 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _26 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _27 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _28 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _29 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _30 = models.ForeignKey(Day, on_delete=models.CASCADE)
+    _31 = models.ForeignKey(Day, on_delete=models.CASCADE)
 
 # Subject to change based on data we can get from Galaxy Digital
 class Volunteer(models.Model):
@@ -41,7 +81,7 @@ class Volunteer(models.Model):
     notify_email = models.BooleanField(default=False)
     notify_text = models.BooleanField(default=False)
     notify_call = models.BooleanField(default=False)
-    availability = models.JSONField(default=dict)
+    availability = models.ForeignKey(Availability, on_delete=models.CASCADE)
     current_appointments = models.JSONField(default=dict, editable=False)
     additional_notes = models.TextField(default='N/A')
 
