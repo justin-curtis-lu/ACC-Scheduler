3fd9718b5043652794fd5d9f83bcd576ec4cee08
Nate Yeo <nathan@theyeos.com>
2021-09-06 09:03:43 -0700

    fixed availability repeating

--- ACC_scheduler/scheduling_application/views.py
+++ ACC_scheduler/scheduling_application/views.py
@@ -566,7 +566,7 @@ def view_availability(request, pk):
     # data = {'form-TOTAL_FORMS': '31', 'form-INITIAL_FORMS': '0', 'form-MAX_NUM_FORMS': ''}
     formset = DayFormSet(initial=[data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12,
                                   data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23,
-                                  data24, data25, data26, data27, data28, data29, data30, data31], queryset=Day.objects.none())
+                                  data24, data25, data26, data27, data28, data29, data30, data31], queryset=volunteer.Days.all())
 
 
     if request.method == "POST":
