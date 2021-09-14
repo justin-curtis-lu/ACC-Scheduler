985a889fe96a7013a6903aa9ba097a1f02d39071
Justin <justin.curtis.lu@gmail.com>
2021-09-14 16:35:11 -0700

    changes

--- ACC_scheduler/ACC_scheduler/settings.py
+++ ACC_scheduler/ACC_scheduler/settings.py
@@ -21,13 +21,13 @@ from django.core.management.utils import get_random_secret_key
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
-# MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-DEBUG = os.getenv("DEBUG", "False") == "True"
+
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
@@ -78,6 +78,8 @@ WSGI_APPLICATION = 'ACC_scheduler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 if DEVELOPMENT_MODE is True:
