045a3e9ca5acedec8f0f7975eb542da5974a6ff2
Justin <justin.curtis.lu@gmail.com>
2021-09-15 22:47:45 -0700

    test original port

--- ACC_scheduler/settings.py
+++ ACC_scheduler/settings.py
@@ -160,7 +160,7 @@ if DEVELOPMENT_MODE is False:
 
 # Email Settings
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '465'
+EMAIL_PORT = '587'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
